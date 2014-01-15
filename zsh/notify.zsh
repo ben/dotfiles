1d38eb9f2a111880b649b651dd2fc144c29878ff
Ben Straub <bs@github.com>
2014-01-15 06:09:01 -0800

    Fix terminal-notify

--- zsh/notify.zsh
+++ zsh/notify.zsh
@@ -2,5 +2,5 @@ notify() {
 	RBENV_VERSION=`rbenv global`
 	local msg="${*}"
 	terminal-notifier -name 'Terminal' -message $msg -group TerminalNotifier > NULL
-	terminal-notifier -remove TerminalNotifier > NULL
+	#terminal-notifier -remove TerminalNotifier > NULL
 }

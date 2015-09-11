77dea2d77fa39220d0638ee362c507adc7369968
Ben Straub <ben@straub.cc>
2015-09-11 09:46:35 -0700

    TFS aliases

---
+++ zsh/tfs.zsh
@@ -0,0 +1,3 @@
+export PATH=~/bin/tfs:$PATH
+alias tfs='tf -login:snd\\benstraub_cp,traderjoe -server:https://tfs.codeplex.com:443/tfs/TFS13'
+# alias tfs='tf -login:benstraub,Traderj0e -server:https://benstraub.visualstudio.com/DefaultCollection'

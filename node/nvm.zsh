21572cdb4d4d1a4a6769176fe907489f377f4423
Ben Straub <ben@straub.cc>
2018-01-26 15:52:27 -0800

    Use rbenv and nodenv

--- node/nvm.zsh
+++
@@ -1,2 +0,0 @@
-export NVM_DIR="/Users/bstraub/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
--- ruby/chruby.zsh
+++
@@ -1,4 +0,0 @@
-if [[ -f /usr/local/opt/chruby/share/chruby/chruby.sh ]] then
-  source /usr/local/opt/chruby/share/chruby/chruby.sh
-  source /usr/local/opt/chruby/share/chruby/auto.sh
-fi
